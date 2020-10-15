2cd99fa96a63cee2822b6f7b9669078a0a771b83
Drnasin208 <60597312+Drnasin208@users.noreply.github.com>
2020-10-16 00:30:47 +0200

    First commit

---
+++ js/restoran.js
@@ -0,0 +1,12 @@
+
+ $('div').on('click', function() {
+     $(this).toggleClass('show-description');
+  });
+
+  $('h1').on('click', function() {
+      $('body').toggleClass('back');
+  });
+
+  $('img').on('click', function() {
+      $('img').css('border-radius','20px');
+  });
